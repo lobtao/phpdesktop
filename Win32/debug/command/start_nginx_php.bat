dfc681114c1a79eac8ad1a5b7e175cf851f81764
lobtao <lobtao@qq.com>
2019-02-23 20:35:28 +0800

    support nginx

--- Win32/debug/command/start_nginx_php.bat
+++ Win32/debug/command/start_nginx_php.bat
@@ -11,7 +11,7 @@ REM set PHP_FCGI_CHILDREN=5
 REM 每个进程处理的最大请求数，或设置为 Windows 环境变量
 set PHP_FCGI_MAX_REQUESTS=1000
 
-REM RunHiddenConsole.exe %php_home%/phpdesktop_php.exe init.php
+RunHiddenConsole.exe %php_home%/phpdesktop_php.exe init.php
 
 echo Starting nginx...
 RunHiddenConsole.exe %nginx_home%/phpdesktop_nginx.exe -p %nginx_home%
