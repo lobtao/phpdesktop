8ee1d29f6d9d68feb16b2119a8a2af70f5dab704
lobtao <lobtao@qq.com>
2019-02-23 18:27:19 +0800

    support nginx

--- Win32/debug/command/start_nginx_php.bat
+++ Win32/debug/command/start_nginx_php.bat
@@ -6,7 +6,7 @@ set php_home=../php_7.0.9
 set nginx_home=../nginx_1.14.2
 
 REM Windows 下无效
-REM set PHP_FCGI_CHILDREN=5
+REM set PHP_FCGI_CHILDREN=
 
 REM 每个进程处理的最大请求数，或设置为 Windows 环境变量
 set PHP_FCGI_MAX_REQUESTS=1000
